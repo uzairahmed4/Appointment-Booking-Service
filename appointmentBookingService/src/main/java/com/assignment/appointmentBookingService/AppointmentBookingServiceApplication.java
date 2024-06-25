ad8c506eee43cf25c85d8b2a9af1a689015adfe5
Mohammed Uzair Ahmed <97060605+uzairahmed4@users.noreply.github.com>
2024-06-26 02:24:28 +0400

    Add files via upload

---
+++ appointmentBookingService/src/main/java/com/assignment/appointmentBookingService/AppointmentBookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.appointmentBookingService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentBookingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmentBookingServiceApplication.class, args);
+	}
+
+}
